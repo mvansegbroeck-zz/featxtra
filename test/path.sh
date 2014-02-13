39848cfee8a798699a0cb442b2bae4ba95b3697b
Maarten Van Segbroeck <maarten@lisa.usc.edu>
2014-02-13 00:12:51 -0800

    commit 2/12/14

--- test/path.sh
+++
@@ -1,3 +0,0 @@
-curdir=$PWD
-export PATH=$PWD/../feat:$PWD/../featbin:$PWD/../transform:$PWD/../../src/bin:$PWD/../../tools/openfst/bin:$PWD/../../src/fstbin/:$PWD/../../src/gmmbin_mdt/:$PWD/../../src/featbin/:$PWD/../../src/lm/:$PWD/../../src/sgmmbin/:$PWD/../../src/fgmmbin/:$PWD/../../src/latbin/:$PWD:$PATH
-export LC_ALL=C
