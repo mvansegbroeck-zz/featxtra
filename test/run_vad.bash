d6cb8c31cc7c6ce0dcfdb3f138a0f99ed1a2f8fc
Zisis Iason Skordilis <zisis.skordilis@gmail.com>
2015-02-14 06:04:22 -0800

    Fixed and cleaned up run_vad.bash script
    - Add processing of raw pitch feature stream to actual voicing probability so
      that the VAD uses POV as feature and not NFCC (which is wrong)
    - Remove unused option --ar-order from apply-ltsv
    - Include option --length-tolerance=1 in paste-feats because kaldi's pitch
      extractor does not output the same number of frames as other feature
      extractors for some combinations of frame length / overlap and signal length.
      Found no other way to fix this than to ignore the (sometimes present) extra
      frame.

--- test/run_vad.bash
+++ test/run_vad.bash
@@ -9,9 +9,10 @@ export LC_ALL=C
 wavin=in/wav
 
 compute-kaldi-pitch-feats --sample-frequency=16000 scp:$wavin.scp ark:- | \
-   extract-dims --start=1 --end=1 ark:- ark:out/vprob.ark
+   apply-nccf-to-pov ark:- ark:- | \
+      extract-dims --start=1 --end=1 ark:- ark:out/vprob.ark
 compute-gtf-feats --verbose=2 --config=conf/gtf.conf scp:$wavin.scp ark:- | \
    apply-arma --ar_order=5 ark:- ark:- | \
-      apply-ltsv --ar_order=2 ark:- ark:out/ltsv.ark
-paste-feats ark:out/ltsv.ark ark:out/vprob.ark ark:- | \
+      apply-ltsv ark:- ark:out/ltsv.ark
+paste-feats --length-tolerance=1 ark:out/ltsv.ark ark:out/vprob.ark ark:- | \
    apply-vad --ctx-win=40 --vad-thr=0.2 ark:- ark:out/vad.ark
