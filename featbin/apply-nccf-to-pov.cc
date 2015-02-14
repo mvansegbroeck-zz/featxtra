e04ad349eee60b32cfe96039fa51a595c06cfeea
Zisis Iason Skordilis <zisis.skordilis@gmail.com>
2015-02-14 05:50:52 -0800

    Added utility to convert NCCF to Probability of Voicing (POV)
    - Kaldi's pitch extractor outputs (NCCF, pitch). Add module to convert
      NCCF to voicing probability (POV). This is a wrapper around kaldi's
      NccfToPov (defined in feat/pitch-functions.cc). To make this work,
      NccfToPov's declaration has to be added to feat/pitch-functions.h
      (For convenience a copy of the modified pitch-functions.h has been
      previously included in the repo).

---
+++ featbin/apply-nccf-to-pov.cc
@@ -0,0 +1,81 @@
+// featbin/apply-nccf-to-pov.cc
+
+// Copyright 2009-2011  Microsoft Corporation
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+// WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+// See the Apache 2 License for the specific language governing permissions and
+// limitations under the License.
+
+#include "base/kaldi-common.h"
+#include "util/common-utils.h"
+#include "matrix/kaldi-matrix.h"
+#include "feat/pitch-functions.h"
+#include "transform/featxtra-functions.h"
+#include <iostream>
+using namespace std;
+
+namespace kaldi {
+
+  void ApplyNccfToPov(Matrix<BaseFloat>* raw_kaldi_pitch_feats);
+
+}
+
+int main(int argc, char *argv[]) {
+  try {
+    using namespace kaldi;
+
+    const char *usage =
+        "Transform raw NCCF to accurate Probability of Voicing (POV).\n"
+        "Kaldi pitch extractor (compute-kaldi-pitch) outputs (NCCF, pitch).\n"
+        "This program transforms (NCCF, pitch) -> (POV, pitch).\n";
+
+    ParseOptions po(usage);
+
+    po.Read(argc, argv);
+
+    if (po.NumArgs() != 2) {
+      po.PrintUsage();
+      exit(1);
+    }
+
+    kaldi::int32 num_done = 0;
+    
+    std::string feat_rspecifier = po.GetArg(1);
+    std::string feat_wspecifier = po.GetArg(2);
+
+    SequentialBaseFloatMatrixReader feat_reader(feat_rspecifier);
+    BaseFloatMatrixWriter feat_writer(feat_wspecifier);
+    
+    for (;!feat_reader.Done(); feat_reader.Next()) {
+      std::string utt = feat_reader.Key();
+      Matrix<BaseFloat> feat(feat_reader.Value());
+      ApplyNccfToPov(&feat);
+      feat_writer.Write(utt, feat);
+      num_done++;
+    }
+    return (num_done != 0 ? 0 : 1);
+  } catch(const std::exception &e) {
+    std::cerr << e.what();
+    return -1;
+  }
+}
+
+namespace kaldi {
+
+void ApplyNccfToPov(Matrix<BaseFloat>* raw_kaldi_pitch_feats) {
+  MatrixIndexT num_frames = raw_kaldi_pitch_feats->NumRows();
+  for (MatrixIndexT frame = 0; frame < num_frames; ++frame) {
+    (*raw_kaldi_pitch_feats)(frame, 0) = NccfToPov((*raw_kaldi_pitch_feats)(frame, 0));
+  }
+}
+
+}
